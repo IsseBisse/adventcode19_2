a7fc7fa9682e2ebc4819b2073e6d1d59ac4c5a03
Isak Liljequist <isak.liljequist@gmail.com>
2021-05-17 13:42:40 +0200

    Added template

---
+++ template/main.py
@@ -0,0 +1,14 @@
+def get_data(path):
+	with open(path) as file:
+		data = file.read().split("\n")
+
+	return data
+
+def part_one():
+	data = get_data("input.txt")
+
+def part_two():
+	data = get_data("input.txt")
+
+if __name__ == '__main__':
+	part_one()
